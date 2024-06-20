02111541c21e73a084fe3608b54e7b70e4408d1f
Nekuko <21350635+Nekuko@users.noreply.github.com>
2024-06-20 23:29:49 +0100

    apo info page

--- src/components/Filters/menus/CharacterDropdown.js
+++ src/components/Filters/menus/CharacterDropdown.js
@@ -143,6 +143,89 @@ function CharacterDropdown({ dropdownName, dropdownStates, setDropdownStates, na
     });
   }, [characters, filters]);
 
+
+  // Handler for subattribute checkbox
+  const handleSubattributeCheck = (character, attribute, subattribute) => {
+    const isSubattributeChecked = !characters[character]?.[attribute]?.[subattribute]?.checked;
+    const updatedSubattributes = {
+      ...characters[character][attribute],
+      [subattribute]: {
+        ...characters[character][attribute][subattribute],
+        checked: isSubattributeChecked
+      }
+    };
+
+    // Update the state with the new subattributes
+    setDropdownStates(prevState => ({
+      ...prevState,
+      [dropdownName]: {
+        ...prevState[dropdownName],
+        characters: {
+          ...prevState[dropdownName].characters,
+          [character]: {
+            ...prevState[dropdownName].characters[character],
+            [attribute]: updatedSubattributes
+          }
+        }
+      }
+    }));
+  };
+
+  // Handler for attribute dropdown click
+  const handleAttributeDropdownClick = (character, attribute) => {
+    const isOpen = characters[character]?.[attribute]?.open;
+    const updatedAttributes = {
+      ...characters[character],
+      [attribute]: {
+        ...characters[character][attribute],
+        open: !isOpen
+      }
+    };
+
+    // Update the state with the new attributes open state
+    setDropdownStates(prevState => ({
+      ...prevState,
+      [dropdownName]: {
+        ...prevState[dropdownName],
+        characters: {
+          ...prevState[dropdownName].characters,
+          [character]: updatedAttributes
+        }
+      }
+    }));
+  };
+
+  // Handler for subattribute dropdown click (if needed)
+  const handleSubattributeDropdownClick = (character, attribute, subattribute) => {
+    const isOpen = characters[character]?.[attribute]?.[subattribute]?.open;
+    const updatedSubattributes = {
+      ...characters[character][attribute],
+      [subattribute]: {
+        ...characters[character][attribute][subattribute],
+        open: !isOpen
+      }
+    };
+
+    // Update the state with the new subattributes open state
+    setDropdownStates(prevState => ({
+      ...prevState,
+      [dropdownName]: {
+        ...prevState[dropdownName],
+        characters: {
+          ...prevState[dropdownName].characters,
+          [character]: {
+            ...prevState[dropdownName].characters[character],
+            [attribute]: updatedSubattributes
+          }
+        }
+      }
+    }));
+  };
+
+
+
+
+
   return (
     <div className="dropdown" ref={dropdownRef}>
       <div onClick={handleGroupClick}>
@@ -160,8 +243,8 @@ function CharacterDropdown({ dropdownName, dropdownStates, setDropdownStates, na
               <div key={index}>
                 <div className="item-header">
                   <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <span 
-                      className={`character-title ${characters[character]?.checked ? '' : 'dimmed'}`} 
+                    <span
+                      className={`character-title ${characters[character]?.checked ? '' : 'dimmed'}`}
                       onClick={() => !isDisabled && handleCharacterCheck(character)}
                     >
                       {character}
@@ -180,19 +263,61 @@ function CharacterDropdown({ dropdownName, dropdownStates, setDropdownStates, na
                 </div>
                 {characters[character]?.open && (
                   <div className="attribute-list">
-                    {characterAttributes.map((attribute, index) => (
-                      <div key={index} className="attribute-item">
-                        <span className={characters[character]?.[attribute] ? "attribute-unchecked" : "attribute-checked"}>
-                          {attribute}
-                        </span>
-                        <input
-                          type="checkbox"
-                          checked={!!characters[character]?.[attribute]}
-                          onChange={() => !isDisabled && handleAttributeCheck(character, attribute)}
-                          disabled={isDisabled}
-                        />
-                      </div>
-                    ))}
+                    {characterAttributes.map((attribute, index) => {
+                      const subAttributes = Object.keys(characters[character][attribute]).filter(subAttribute => subAttribute !== 'checked' && subAttribute !== 'open');
+                      const hasAttributes = subAttributes.length > 0;
+                      return (
+                        <div key={index} className="item-header">
+                          {hasAttributes ? (
+                            // Render dropdown menu for attributes with subattributes
+                            <div className="dropdown">
+                              <div onClick={() => !isDisabled && handleAttributeDropdownClick(character, attribute)}>
+                                <span
+                                  className={`attribute-title ${characters[character]?.[attribute]?.checked ? '' : 'dimmed'}`}
+                                >
+                                  {attribute}
+                                </span>
+                                <FontAwesomeIcon className="dropdown-icon" icon={characters[character]?.[attribute]?.open ? faChevronUp : faChevronDown} />
+                              </div>
+                              {characters[character]?.[attribute]?.open && (
+                                <div className="dropdown-menu">
+                                  {Object.keys(characters[character][attribute]).map((subattribute, subIndex) => (
+                                    <div key={subIndex} className="attribute-item">
+                                      <span
+                                        className={`attribute-title ${characters[character]?.[attribute]?.[subattribute]?.checked ? '' : 'dimmed'}`}
+                                        onClick={() => !isDisabled && handleSubattributeDropdownClick(character, attribute, subattribute)}
+                                      >
+                                        {subattribute}
+                                      </span>
+                                      {characters[character]?.[attribute]?.[subattribute]?.open && (
+                                        // Render interactive elements for subattributes here
+                                        <input
+                                          type="checkbox"
+                                          checked={!!characters[character]?.[attribute]?.[subattribute]?.checked}
+                                          onChange={() => !isDisabled && handleSubattributeCheck(character, attribute, subattribute)}
+                                          disabled={isDisabled}
+                                        />
+                                      )}
+                                    </div>
+                                  ))}
+                                </div>
+                              )}
+                            </div>
+                          ) : (
+                            // Render checkbox for attributes without subattributes
+                            <span className={characters[character]?.[attribute]?.checked ? "attribute-unchecked" : "attribute-checked"}>
+                              {attribute}
+                            </span>
+                          )}
+                          <input
+                            type="checkbox"
+                            checked={!!characters[character]?.[attribute]?.checked}
+                            onChange={() => !isDisabled && handleAttributeCheck(character, attribute)}
+                            disabled={isDisabled}
+                          />
+                        </div>
+                      );
+                    })}
                   </div>
                 )}
               </div>
