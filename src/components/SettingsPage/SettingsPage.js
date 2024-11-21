bfc8df3ad153185230e931c60f74578e97c04616
Nekuko <21350635+Nekuko@users.noreply.github.com>
2024-11-21 15:45:40 +0000

    2-8

--- src/components/SettingsPage/SettingsPage.js
+++ src/components/SettingsPage/SettingsPage.js
@@ -45,7 +45,7 @@ function populateOrders() {
     correctKeys.push(`ssc_p2_c2-${i}`)
   }
 
-  for (let i = 1; i <= 7; i++) {
+  for (let i = 1; i <= 8; i++) {
     correctOrder.push(`Seven Shadow Chronicles Part 3, Chapter 3-${i}`);
     correctKeys.push(`ssc_p3_c3-${i}`)
   }
